a1dc75efa6a11594cd742da21e097feffaa24849
djenaman <dipak.cvrca@gmail.com>
2017-05-25 18:34:05 +0530

    added new file demo2.java in repo directly

---
+++ GitDemo/src/demo2.java
@@ -0,0 +1,11 @@
+public class demo2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("this is a test2.1");
+		System.out.println("this is a test3.1");
+		System.out.println("this is a test4.1");
+
+	}
+
+}
